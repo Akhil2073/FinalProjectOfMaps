cc147b03cf33ee14ea18c7bbf2d29ddd0deb9d04
Akhilesh Reddy <akhilesh6567@gmail.com>
2022-12-12 11:58:19 -0500

    fragments added

---
+++ app/src/main/java/com/android/mapsproject/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.android.mapsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<MapsFragment>(R.id.fragment_container_view)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.getItemId()
+
+        if (id == R.id.menu_map) {
+            return true
+        }
+        if (id == R.id.menu_places) {
+            return true
+        }
+        if (id == R.id.menu_email) {
+            return true
+        }
+        if (id == R.id.menu_about) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+
+    }
+
+}
